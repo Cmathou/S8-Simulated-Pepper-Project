181f2f80293e67dffe30cdafd963a6ebe152925f
Cmathou <47972053+Cmathou@users.noreply.github.com>
2019-06-04 16:52:27 +0200

    Update detection.py

--- script/detection.py
+++ script/detection.py
@@ -1,16 +1,36 @@
-imfile = "data://Benjaminlegrosbg/images/avion.jpg"
+import Algorithmia
 
-if client.file(imfile).exists() is False:
-    # Upload local file
-    client.file(imfile).putFile("avion.jpg")
+class detection:
+    def detect(self,path):
 
-input = {
-  "image": imfile,
-  "output": "data://.algo/deeplearning/ObjectDetectionCOCO/temp/suit_and_tie.png",
-  "min_score": 0.7,
-  "model": "ssd_mobilenet_v1"
-}
+        client = Algorithmia.client('simhBkZ0DJroYl2ZNAJsJKEfZWU1')
 
-algo = client.algo('deeplearning/ObjectDetectionCOCO/0.2.1')
-algo.set_options(timeout=300) # optional
-print(algo.pipe(input).result)
+        imfile = "data://Benjaminlegrosbg/images/detection.png"
+
+        # Upload local file
+        client.file(imfile).putFile(path)
+
+        input = {
+          "image": imfile,
+          "output": "data://.algo/deeplearning/ObjectDetectionCOCO/temp/detection.png",
+          "min_score": 0.7,
+          "model": "ssd_mobilenet_v1"
+        }
+
+        algo = client.algo('deeplearning/ObjectDetectionCOCO/0.2.1')
+        algo.set_options(timeout=300) # optional
+        
+        if len(algo.pipe(input).result[u'boxes']) > 3:
+	        type_obj = []
+	        coord = []
+	        for obj in algo.pipe(input).result[u'boxes']:
+		        type_obj.append(obj[u'label'])
+		        coord.append(obj[u'coordinates'])
+	        return [type_obj,coord]
+        else:
+	        type_obj = []
+	        coord = []
+	        for obj in algo.pipe(input).result[u'boxes']:
+		        type_obj.append(obj[u'label'])
+		        coord.append(obj[u'coordinates'])
+	        return [type_obj,coord]
